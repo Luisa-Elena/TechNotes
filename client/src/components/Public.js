abafa6a138e02b8a27501565da6b2403298bca31
Luisa <luisa21elena@gmail.com>
2023-12-28 16:06:30 +0200

    first commit

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -8,29 +8,11 @@ export default function Public() {
             <h1>TechNotes</h1>
             <h2>About</h2>
             <p> 
-                Replace current sticky note system
-                Add a public facing page with basic contact info
-                Add an employee login to the notes app
-                Provide a welcome page after login
-                Provide easy navigation
-                Display current user and assigned role
-                Provide a logout option
-                Require users to login at least once per week
-                Provide a way to remove employee access asap if needed
-                Notes are assigned to specific employees
-                Notes have a ticket #, title, note body, created & updated dates
-                Notes are either OPEN or COMPLETED
-                Users can be Employees, Managers, or Admins
-                Notes can only be deleted by Managers or Admins
-                Anyone can create a note (when customer checks-in)
-                Employees can only view and edit their assigned notes
-                Managers and Admins can view, edit, and delete all notes
-                Only Managers and Admins can access User Settings
-                Only Managers and Admins can create new users
-                Desktop mode is most important but should be available in mobile
+                This website is actually like an employee management system for a small tech repair shop business, used to keep track of the tasks for each employee.
+                This project also implements role based permissions. Each user can be either an Admin, Manager or Employee. An employee can only view and edit its assigned notes. Anyone can create a note (when a customer checks-in) which will be assigned to a specific employee. Notes can only be deleted by managers or admins. They can view, edit and delete all notes and only they can acces user setings and even add new users (create new accounts).
             </p>
             <h2>Contact</h2>
-            <p>put some contact data here - address, phone number, email</p>
+            <p>put some contact data here - address, phone number, email for the repair shop</p>
             <h2>Login</h2>
             <Link to="/login" className='login-link'>Employee Login</Link>
         </div>
